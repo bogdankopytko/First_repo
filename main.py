d44cdcdeae9ab757686ef75d07b6f986d28836af
bodyk <bo.kopytko@gmail.com>
2024-04-21 16:39:27 +0200

    program to count hours minutes and seconds

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Hello World!")
-print("Hello Git!")
+n = 5000
+
+hours = n // (60 * 60)
+minutes = (n - hours * 60 * 60) // 60
+seconds = n - hours * 60 * 60 - minutes * 60
+
+print(f"hours - {hours}, minutes - {minutes}, seconds - {seconds}")
